d74eb51d2dce567598087c925a3cd1e7943058cb
David Torres <david.torres20022511@gmail.com>
2024-12-21 20:56:45 -0300

    Colores
    Se agregaron colores para la aplicacion web

--- web-digimon/App.tsx
+++ web-digimon/App.tsx
@@ -3,24 +3,28 @@ import { StyleSheet, View, Button, Text } from 'react-native';
 import { Card } from 'react-native-paper';
 
 export default function App() {
-  const [file, setFile] = useState<any>(null);
-  const [digimonName, setDigimonName] = useState<string>('');
+  const [file, setFile] = useState<any>(null); 
+  const [digimonName, setDigimonName] = useState<string>(''); 
+  const [imagePreview, setImagePreview] = useState<string | null>(null); 
 
-  const url = 'https://dry-sands-47061-8d2e711f5b8e.herokuapp.com/prediccion'; // Cambia la URL si es necesario
+  const url = 'https://dry-sands-47061-8d2e711f5b8e.herokuapp.com/prediccion/';
 
   const getDigimon = async (file: any) => {
     try {
       const formData = new FormData();
       formData.append('file', file);
 
-      const response = await fetch('http://localhost:8000/prediccion', {
+      const response = await fetch(url, {
         method: 'POST',
         body: formData,
       });
 
+      if (!response.ok) {
+        throw new Error('Error en la solicitud');
+      }
+
       const digimon = await response.json();
-      console.log(digimon);
-      setDigimonName(digimon.Nombre); // Mostrar el nombre del Digimon
+      setDigimonName(digimon.Nombre);
     } catch (error) {
       console.error('Error al enviar la imagen:', error);
     }
@@ -30,20 +34,34 @@ export default function App() {
     const selectedFile = e.target.files ? e.target.files[0] : null;
     if (selectedFile) {
       setFile(selectedFile);
+
+      // Crear una URL temporal para mostrar la imagen seleccionada
+      const imageUrl = URL.createObjectURL(selectedFile);
+      setImagePreview(imageUrl);
     }
   };
 
   return (
     <View style={styles.container}>
-      <Card>
-        <Card.Title title="Encuentra tu Digimon" />
-        <Card.Content>
-          <input type="file" onChange={handleFileChange} style={{marginBottom:5}} />
+      <Card style={{alignSelf: "center", backgroundColor: "gold"}}>
+        <Card.Title title="Encuentra tu Digimon" style= {{alignSelf:"center"}} />
+        <Card.Content style={{alignItems: "center"}}>
+          <input type="file" onChange={handleFileChange} style={{marginBottom: 5}}/>
+          
+          {imagePreview && (
+            <div style={{margin: 5}}>
+              <img 
+                src={imagePreview} 
+                alt="Vista previa" 
+                style={{ width: '200px', height: '200px'}}
+              />
+            </div>
+          )}
           <Button
             title="Enviar Imagen"
             onPress={() => file && getDigimon(file)}
           />
-          {digimonName && <Text>{digimonName}</Text>}
+          {digimonName && <Text style={{marginTop: 5}}>{digimonName}</Text>}
         </Card.Content>
       </Card>
     </View>
@@ -53,7 +71,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#60b4ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
